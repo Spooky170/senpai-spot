cc515f658075b475b86b6d11d1cecce12356cd56
Senpai Spot <bhupendrajangu7@gmail.com>
2026-05-15 15:53:05 -0700

    Remove @sanity/vision from config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemaTypes'
 
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
   basePath:  '/studio',
   plugins: [
     structureTool(),
-    visionTool(),
   ],
   schema: {
     types: schemaTypes,
